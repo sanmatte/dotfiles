7f184a338712b0e302e4997f895ec131b852f20c
sanmatte <matteee.sanna@gmail.com>
2025-05-20 02:04:27 +0200

    major fix to scritps now get_terminal and get_browser will bring the window if is not in the current workspace, if instead is in the current workspace it will send back to their usual workspace

--- .config/aerospace/scripts/get_browser.sh
+++ .config/aerospace/scripts/get_browser.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# move terminal to current workspace
-TARGET=$(aerospace list-workspaces --focused)
-aerospace workspace 2
-aerospace move-node-to-workspace "$TARGET"
-aerospace workspace "$TARGET"
+# move browser to current workspace
+BROWSER_BUNDLE_ID="app.zen-browser.zen"
+CURRENT_WORKSPACE=$(aerospace list-workspaces --focused)
+
+# get browser window and workspace id
+IFS='-' read -r BROWSER_ID BROWSER_WORKSPACE <<< "$(aerospace list-windows --monitor all --app-bundle-id "$BROWSER_BUNDLE_ID" --format '%{window-id}-%{workspace}')"
+
+# if the browser is not in the current workspace, bring it; else send to workspace 2 
+if [[ "$CURRENT_WORKSPACE" != "$BROWSER_WORKSPACE" ]]; then
+    TARGET=$CURRENT_WORKSPACE
+else
+    TARGET=2
+fi
+
+aerospace move-node-to-workspace --window-id "$BROWSER_ID" "$TARGET"
+
--- .config/aerospace/scripts/get_terminal.sh
+++ .config/aerospace/scripts/get_terminal.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 # move terminal to current workspace
-TARGET=$(aerospace list-workspaces --focused)
-aerospace workspace 1
-aerospace move-node-to-workspace "$TARGET"
-aerospace workspace "$TARGET"
+ITERM_BUNDLE_ID="com.googlecode.iterm2"
+CURRENT_WORKSPACE=$(aerospace list-workspaces --focused)
+
+# get terminal and workspace id
+IFS='-' read -r TERMINAL_ID TERMINAL_WORKSPACE <<< "$(aerospace list-windows --monitor all --app-bundle-id "$ITERM_BUNDLE_ID" --format '%{window-id}-%{workspace}')"
+
+# if the terminal is not in the current workspace, bring it; else send to workspace 1
+if [[ "$CURRENT_WORKSPACE" != "$TERMINAL_WORKSPACE" ]]; then
+    TARGET=$CURRENT_WORKSPACE
+else
+    TARGET=1
+fi
+
+aerospace move-node-to-workspace --window-id "$TERMINAL_ID" "$TARGET"
+
