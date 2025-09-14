5415c74a49ae36e75a4931eaa016a97f6a6087a2
sanmatte <matteee.sanna@gmail.com>
2025-09-15 01:40:23 +0200

    notebooks and copyright for projects

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,21 @@
 require("configs")
+vim.g.python3_host_prog = vim.fn.exepath 'python3'
+vim.g.loaded_python3_provider = nil
+vim.cmd('runtime! plugin/rplugin.vim')
+
+local project_path = vim.fn.expand("~/Github/unwhoop/*")
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = project_path,
+  callback = function()
+    local date = os.date("%Y-%m-%d")
+    local year = os.date("%Y")
+    vim.api.nvim_buf_set_lines(0, 0, 0, false, {
+			"# Unwhoop",
+      "# Created: " .. date,
+      "# Copyright (c) " .. year .. "Matteo Sanna",
+      ""
+    })
+  end
+})
+
